3c2d0aba425f1ed0f81e7f5cace719b965820300
fatma-brahmi <brahmifatma96@gmail.com>
2021-09-28 22:13:54 +0100

    Api

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
+
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 import './App.css';
 
+import UserList from './Components/UserList';
+
+
+
 function App() {
+  const [users, setUsers] = useState([])
+  useEffect(() => {
+    axios.get("https://jsonplaceholder.typicode.com/users").then((result) => {
+      setUsers(result.data);
+    });
+  }, []);
+  
+ 
+    
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+        <UserList users={users}  />
+      
     </div>
   );
 }
---
+++ src/Components/UserList.js
@@ -0,0 +1,46 @@
+import React from "react";
+import { Card, ListGroup, ListGroupItem } from "react-bootstrap";
+
+const UserList = ({ users }) => {
+  return (
+    <div>
+         <div className="user">
+      {users.map((user) => (
+       
+            <Card  className='carte' style={{ width: "30rem" }}>
+          <Card.Body>
+            <Card.Title className='title'>
+              {user.name}
+
+              {user.username}{" "}
+            </Card.Title>
+          </Card.Body>
+          <ListGroup className="list-group-flush">
+            <ListGroupItem><u>Email:</u> {user.email}</ListGroupItem>
+            <ListGroupItem>
+             <u> Address:</u>
+              <Card.Text>
+                {user.address.street},{user.address.suite},,{user.address.city},
+                {user.address.zipcode},{user.address.geo.lat}
+                {user.address.geo.lng}
+              </Card.Text>
+            </ListGroupItem>
+            <ListGroupItem><u>Phone: </u>{user.phone} </ListGroupItem>
+            <ListGroupItem><u>Website: </u>{user.website} </ListGroupItem>
+            <ListGroupItem><u>Company:</u>
+                <Card.Text>
+            {user.company.name},{user.company.catchPhrase},{user.company.bs}
+            </Card.Text>
+                 </ListGroupItem>
+          </ListGroup>
+         
+        </Card>
+      
+      ))}
+      </div>
+      ;
+    </div>
+  );
+};
+
+export default UserList;
